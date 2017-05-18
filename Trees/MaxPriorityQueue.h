ea4cd719089187389579e133425d8ec55a8919c3
Ian Chen <ianre657@gmail.com>
2017-05-19 00:09:10 +0800

    finish definition

--- Trees/MaxPriorityQueue.h
+++ Trees/MaxPriorityQueue.h
@@ -1,6 +1,8 @@
 #ifndef _MAX_PQ_H__
 #define _MAX_PQ_H__
+
 #include<cstddef>
+#include <stdexcept>
 
 template< typename T>
 class MaxPriorityQueue{
@@ -15,15 +17,38 @@ class MaxPriorityQueue{
 template<typename T>
 class MaxHeap: public MaxPriorityQueue<T>{
 public:
-    bool IsEmpty() override;
-    const T& Top() override;
+    MaxHeap( int _capacity =10 );
+
+    bool IsEmpty() const override { return heapSize==0; };
+
+    const T& Top() const override { return heap[1]; }; 
+
     void Push( const T&) override ;
-    void Pop() override ;
+
+    void Pop( void ) override ;
+
 protected:
     T * heap;
     size_t heapSize;
     size_t capacity;
 };
 
+template<typename T>
+MaxHeap<T>::MaxHeap( int _capacity ){
+    if( _capacity <1 ) throw std::runtime_error("Capacity must be >=1.");
+    capacity = _capacity;
+    heapSize = 0;
+    heap = new T[capacity+1];// heap[0] is not used.
+}
+
+template<typename T>
+void MaxHeap<T>::Push( const T&){
+    
+}
+
+template<typename T>
+void MaxHeap<T>::Pop( void ){
+    
+}
 
 #endif
